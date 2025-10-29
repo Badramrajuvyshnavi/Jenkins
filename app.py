7964815db2d8028969d11307a559b20c2f6dcf42
Badramrajuvyshnavi <143017660+Badramrajuvyshnavi@users.noreply.github.com>
2025-10-29 10:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ def index():
     return render_template('form.html')
 @app.route('/submit',methods=['POST'])
 def submit():
-    username=request.form['username']
+    username=request.form['name']
     rollno=request.form['rollno']
-    email=request.form['email']
+    
     year=request.form['year']
     return render_template('results.html',username=username,rollno=rollno,email=email,year=year)
 if __name__=='__main__':
